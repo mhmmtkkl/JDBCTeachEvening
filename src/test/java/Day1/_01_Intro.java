b5bcc5e293ffa0628284e4aeeb97e05b920f79fc
mhmmtkkl <makoklu32@gmail.com>
2020-09-22 19:19:28 -0400

    intro

--- src/test/java/Day1/_01_Intro.java
+++ src/test/java/Day1/_01_Intro.java
@@ -1,4 +1,19 @@
 package Day1;
 
 public class _01_Intro {
+
+    /*
+        What is JDBC ?
+
+        JDBC(JAVA DATABASE CONNECTION)
+        JDBC help me to automate mysql database.
+
+       I am sending the query and getting the data from the database and make it is matching with expected result.
+
+
+     */
+
+
+
+
 }
