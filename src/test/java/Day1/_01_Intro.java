eea007f892bed1a1a97373c7edad14a0d844f929
mhmmtkkl <makoklu32@gmail.com>
2020-09-22 19:30:40 -0400

    intro

--- src/test/java/Day1/_01_Intro.java
+++ src/test/java/Day1/_01_Intro.java
@@ -1,5 +1,11 @@
 package Day1;
 
+import org.testng.annotations.BeforeClass;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+
 public class _01_Intro {
 
     /*
@@ -13,7 +19,27 @@ public class _01_Intro {
 
      */
 
+    /*
+        Usually in the interview they can ask about connection statement and result set
 
+
+
+     */
+
+    private Connection connection; // With a connection I am able to connect to my Database need to provide URL , username , password
+    private Statement statement;  //  Once you have a Java Statement instance you can execute either a database
+                                    // query or an database update with it.
+
+
+
+    @BeforeClass
+    public void beforeClass(){
+        String URL ="jdbc:mysql://test.medis.mersys.io:3306/sakila";
+        String username = "technostudy";
+        String password = "zhTPis0l9#$&";
+        
+
+    }
 
 
 }
