41ba6d50a35ed8740c8397dd272e8c4ff022d852
mhmmtkkl <makoklu32@gmail.com>
2020-09-22 20:35:09 -0400

    intro

--- src/test/java/Day1/_02_AbsuluteRelative.java
+++ src/test/java/Day1/_02_AbsuluteRelative.java
@@ -2,11 +2,9 @@ package Day1;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class _02_AbsuluteRelative {
 
@@ -29,7 +27,38 @@ public class _02_AbsuluteRelative {
         connection.close();
     }
 
-    
+    @Test
+    public void testAbsolute() throws SQLException {
+
+        ResultSet rs = statement.executeQuery("SELECT * FROM film_text;");
+
+        /*
+            rs.absolute(5);
+                Instead of typing rs.next 5 times we can type  rs.absolute(5); and it will navigate to 5th location directly
+
+         */
+        rs.absolute(5);
+
+        // get the title and description from the 5th row
+        String title = rs.getString("title");
+        String description = rs.getString("description");
+
+        System.out.println(title +" -- " + description);
+
+        String filmID = rs.getString("film_id");
+        System.out.println("film ID 1 -->  "+filmID);
+
+        String filmID2 = rs.getString(1);
+        System.out.println("film ID 2 -->  "+filmID2);
+
+        int filmIDInt = rs.getInt(1);
+        System.out.println("film ID INT --> "+filmIDInt);
+
+     //   int title5 = rs.getInt(2); // int i1 = "AFRICAN EGG"; exception name is NumberFormatException
+     //   System.out.println(title5);
+
+
+    }
 
 
 }
